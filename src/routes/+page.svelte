2b2c743e4789c8c57fdac0cacc12b73b2b6dbacf
John Stockton <stockton.john@gmail.com>
2024-04-02 19:37:54 -0400

    Tailwind and regex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,76 @@
 <script>
-    const nums = [...Array(9).keys()]
-    let currentNum = ""
-    let previousNum = ""
+	// Variables
+	const calculatorButtons = [
+		'7',
+		'8',
+		'9',
+		' / ',
+		'4',
+		'5',
+		'6',
+		' * ',
+		'1',
+		'2',
+		'3',
+		' - ',
+		'0',
+		'.',
+		'=',
+		' + '
+	];
+	let expression = '';
 
-    const operators = ["+", "-", "*", "/"]
-    let currentOperator = ""
+	// Functions
+	function backspace() {
+		const endIndex = expression[expression.length - 1] == ' ' ? -3 : -1;
+		expression = expression.slice(0, endIndex);
+	}
 
-    function primeOperator(event) {
-        previousNum = currentNum
-        currentNum = ""
-        currentOperator = event.target.textContent
-    }
+	function applyOperator(operator, numA, numB) {
+		switch (operator) {
+			case ' + ':
+				return parseFloat(numA) + parseFloat(numB);
+			case ' - ':
+				return parseFloat(numA) - parseFloat(numB);
+			case ' * ':
+				return parseFloat(numA) * parseFloat(numB);
+			case ' / ':
+				return parseFloat(numA) / parseFloat(numB);
+		}
+	}
 
-    function applyOperator() {
-        switch (currentOperator) {
-            case "+":
-                currentNum = parseFloat(currentNum) + parseFloat(previousNum)
-                break
-            case "-":
-                currentNum = parseFloat(currentNum) - parseFloat(previousNum)
-                break
-            case "*":
-                currentNum = parseFloat(currentNum) * parseFloat(previousNum)
-                break
-            case "-":
-                currentNum = parseFloat(currentNum) / parseFloat(previousNum)
-                break
-        }
-    }
+	function evaluateExpression() {
+		// Validate expression as valid using regex (no parentheses)
+		const validatedExpression = expression.match(
+			/^(-?([0-9]*[.])?[0-9]+) [+\-*/] (-?([0-9]*[.])?[0-9]+)$/g
+		);
+		if (validatedExpression == null) {
+			alert(`${expression} is an invalid arithmetic expression.`);
+			return;
+		}
+
+		// Extract tokens and evaluate
+		const [numA, numB] = expression.match(/-?([0-9]*[.])?[0-9]+/g);
+		const [operator] = expression.match(/ [+\-*/] /);
+		expression = applyOperator(operator, numA, numB);
+	}
+
+	function updateExpression(event) {
+		if (event.target.textContent != '=') {
+			expression += event.target.textContent;
+		} else {
+			evaluateExpression();
+		}
+	}
 </script>
 
-<form>
-    <label>Result: <input value={currentNum}/></label>
-    <br />
-    <button on:click={() => currentNum = "" }>Clear</button>
-    <button on:click={() => currentNum = currentNum.slice(0, -1) }>Delete</button>
-    <br />
-    <br />
-    {#each operators as operator}
-        <button on:click={primeOperator}>{operator}</button>
-        <br />
-    {/each}
-    <br />
-    {#each nums as num}
-        <button on:click={() => currentNum += event.target.textContent}>{num}</button>
-        <br />
-    {/each}
-    <br />
-    <button on:click={applyOperator}>=</button>
-</form>
+<div class="w-1/2 max-w-80 mx-auto py-2 grid grid-cols-4 gap-1">
+	<input class="rounded outline outline-gray-500 text-4xl col-span-3 row-span-2" value={expression} />
+	<button class="col-start-4 bg-blue-500 text-white text-base rounded" on:click={backspace}>Back</button>
+	<button class="col-start-4 bg-red-500 text-white text-base rounded" on:click={() => (expression = '')}>Clear</button>
+	{#each calculatorButtons as calculatorButton}
+		<button class="bg-gray-500 text-white rounded" on:click={updateExpression}
+			>{calculatorButton}</button
+		>
+	{/each}
+</div>
